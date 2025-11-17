c2fe9563cbe9af83a65e937a3e598e218e5ca14d
Radu <aliment4476@gmail.com>
2025-11-17 12:34:03 +0100

    Added enemy movement

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,4 +2,28 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    private Transform tower;
+
+    private float speed = 10f;
+    private float minimumDistanceToTower = 3f;
+    private float yPos;
+
+    private void Awake()
+    {
+        tower = GameObject.FindGameObjectWithTag("Tower").transform;
+        yPos = transform.position.y;
+    }
+
+    private void Update()
+    {
+        if (tower != null)
+        {
+            if (Vector3.Distance(transform.position, tower.position) > minimumDistanceToTower)
+            {
+                Vector3 direction = (tower.position - transform.position).normalized;
+                transform.position += speed * Time.deltaTime * direction;
+                transform.position = new Vector3(transform.position.x, yPos, transform.position.z);
+            }
+        }
+    }
 }
